b6e21815d953efb99d43711ffdcfd4b069d646a5
skmonsterr <ctjoaomartins@gmail.com>
2021-03-31 14:33:52 -0300

    BoardException and Defensive Prog

---
+++ src/main/java/src/boardgame/BoardException.java
@@ -0,0 +1,14 @@
+package src.boardgame;
+
+/**
+ *
+ * @author SkM
+ */
+public class BoardException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    
+    public BoardException( String msg ) {
+        super(msg);
+    }
+    
+}
