1c0bc2d182e7d2fb78ae83bac78cc4358089d10b
skmonsterr <ctjoaomartins@gmail.com>
2021-03-30 22:10:42 -0300

    Project Created

---
+++ src/main/java/src/application/Program.java
@@ -0,0 +1,15 @@
+package src.application;
+
+/**
+ *
+ * @author SkM
+ */
+public class Program {
+
+   
+    public static void main(String[] args) {
+       
+        System.out.println("Hello World!");
+    }
+    
+}
